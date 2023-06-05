0286a45af9fd21d8de49845aec9d316f8238df63
satri <satriayusufrafaly@gmail.com>
2023-06-05 20:45:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment_test/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fragment_test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
